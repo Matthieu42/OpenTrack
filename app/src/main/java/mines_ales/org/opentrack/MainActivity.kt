d18b04987a9afa021d8d3133d4993356b469f890
Matthieu Tinnes <matthieu.tinnes@gmail.com>
2019-03-26 19:29:17 +0100

    Add timer

--- app/src/main/java/mines_ales/org/opentrack/MainActivity.kt
+++ app/src/main/java/mines_ales/org/opentrack/MainActivity.kt
@@ -1,21 +1,26 @@
 package mines_ales.org.opentrack
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import mines_ales.org.opentrack.model.Trip
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var textView: TextView? = null
+    private lateinit var textView: TextView
     private val trip: Trip = Trip("Default")
+    private val mHandler: Handler = Handler()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.durationTextView)
+        this.textView = findViewById(R.id.durationTextView)
     }
 
 
@@ -33,7 +38,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     fun startTrip(v: View){
         (v as Button).setText("Stop")
         this.trip.startTrip()
+        this.startTimerUpdater()
+    }
 
+    fun updateTimer(){
+        this.textView.setText(SimpleDateFormat("mm:ss").format(Date(this.trip.getCurrentTime())))
+    }
+
+    var mStatusChecker: Runnable = object : Runnable {
+        override fun run() {
+            try {
+                updateTimer()
+            } finally {
+                mHandler.postDelayed(this, 1000)
+            }
+        }
+    }
+
+    fun startTimerUpdater() {
+        mStatusChecker.run()
     }
 }
 
