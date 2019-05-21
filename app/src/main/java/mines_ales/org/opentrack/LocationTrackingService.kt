cd966f9dd8775b0e62fc022cde111d9475784b8e
Matthieu42 <matthieu.tinnes@gmail.com>
2019-05-21 14:57:49 +0200

    Add LocationTrackerImplementation

--- app/src/main/java/mines_ales/org/opentrack/LocationTrackingService.kt
+++ app/src/main/java/mines_ales/org/opentrack/LocationTrackingService.kt
@@ -1,8 +1,156 @@
 package mines_ales.org.opentrack
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.location.Location
+import android.support.v4.app.ActivityCompat
+import android.util.Log
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import pub.devrel.easypermissions.EasyPermissions
 
+class LocationTrackingService(private val mActivity: Activity, private val mListener: LocationListener) {
+    private val GPS_REQUEST_CODE: Int = 999
+    private var mLocationCallback: LocationCallback? = null
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+    private var mLocationRequest: LocationRequest? = null
+    // initiliaze th class for create instances ans set callback listeners
+    init {
+        try {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(mActivity)
+            mFusedLocationClient!!.lastLocation
+                    .addOnSuccessListener(mActivity) { location ->
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            mListener.onLocationFound(location)
+                        }
+                    }
+            createLocationRequest()
+            mLocationCallback = object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult?) {
+                    for (location in locationResult!!.locations) {
+                        mListener.onLocationFound(location)
+                    }
+                }
 
-class LocationTrackingService{
+            }
+        } catch (ex: SecurityException) {
+            ex.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        checkGpsSetting()
+    }
 
+    //Here, we assigned required minimum and maximum duration that explained above
+    private fun createLocationRequest() {
+        mLocationRequest = LocationRequest.create()
+        mLocationRequest!!.interval = UPDATE_INTERVAL.toLong()
+        mLocationRequest!!.fastestInterval = FASTEST_INTERVAL.toLong()
+        mLocationRequest!!.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        //mLocationRequest!!.smallestDisplacement = DISPLACEMENT.toFloat()
+    }
 
+    //In the start method, we chacking permissions. It'will invoke a permision required pop if required permission not given
+    fun onStart() {
+        if (!checkRequiredLocationPermission()) {
+            getLastLocation()
+        } else {
+            startLocationUpdates()
+        }
+    }
+
+    fun stopLocationUpdates() {
+        mFusedLocationClient!!.removeLocationUpdates(mLocationCallback!!)
+    }
+
+    fun onPause() {
+        stopLocationUpdates()
+    }
+
+    private fun checkRequiredLocationPermission(): Boolean {
+        val perms = arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+        if (!EasyPermissions.hasPermissions(mActivity, *perms)) {
+            mListener.checkRequiredLocationPermission()
+            return false
+        } else {
+            return true
+        }
+    }
+
+    /**
+     * Check Gps setting IN/OFF state. If GPS OFF it'll show on popup.
+     */
+    private  fun checkGpsSetting() {
+
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(mLocationRequest!!)
+        val task = LocationServices.getSettingsClient(mActivity).checkLocationSettings(builder.build())
+
+        task.addOnCompleteListener { result ->
+            try {
+                result.getResult(ApiException::class.java)
+                // All location settings are satisfied. The client can initialize location
+                // requests here.
+            } catch (exception: ApiException) {
+                when (exception.statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED ->{
+                        val resolvable = exception as ResolvableApiException
+
+                        resolvable.startResolutionForResult(mActivity, GPS_REQUEST_CODE)
+
+                    }
+                    // Location settings are not satisfied. But could be fixed by showing the
+                    // user a dialog.
+                    // Cast to a resolvable exception.
+                    LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                        //empty
+                    }
+                }
+                // Show the dialog by calling startResolutionForResult(),
+                // Location settings are not satisfied. However, we have no way to fix the
+                // settings so we won't show the dialog.
+            }
+        }
+    }
+
+    private fun getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(mActivity,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mActivity,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        mFusedLocationClient!!.lastLocation
+                .addOnCompleteListener(mActivity) { task ->
+                    if (task.isSuccessful && task.result != null) {
+                        mListener.onLocationFound(task.result)
+                    } else {
+                        Log.w("", "no_location_detected", task.exception)
+                    }
+                }
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(mActivity,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        mActivity,Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            return
+        }
+        mFusedLocationClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback!!, null)
+    }
+
+    interface LocationListener {
+        fun onLocationFound(location: Location?)
+        fun locationError(msg: String)
+        fun checkRequiredLocationPermission(): Boolean
+    }
+
+    companion object {
+        private const val UPDATE_INTERVAL = 1000 // 10 min
+        private const val FASTEST_INTERVAL = 1000 // 5 min
+        private const val DISPLACEMENT = 10 // 1 km
+        const  val RUN_TIME_PERMISSION_CODE = 999
+    }
 }
--- app/src/main/java/mines_ales/org/opentrack/MainActivity.kt
+++ app/src/main/java/mines_ales/org/opentrack/MainActivity.kt
@@ -1,5 +1,6 @@
 package mines_ales.org.opentrack
 
+import android.location.Location
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
@@ -11,17 +12,20 @@ import mines_ales.org.opentrack.model.Trip
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener,LocationTrackingService.LocationListener {
 
     private lateinit var durationTextView: TextView
     private var trip: Trip = Trip("Default")
     private val mHandler: Handler = Handler()
-    private val locationTrackingService: LocationTrackingService = LocationTrackingService()
+    private lateinit var locationTrackingService: LocationTrackingService
+    private var mPosition: Location? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.durationTextView = findViewById(R.id.durationTextView)
+        this.locationTrackingService = LocationTrackingService(this,this)
+
     }
 
 
@@ -69,5 +73,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     fun startTimerUpdater() {
         mStatusChecker.run()
+    }
+    override fun onLocationFound(location: Location?) {
+        locationTrackingService.stopLocationUpdates()
+        mPosition = location
+        findViewById<TextView>(R.id.avgSpeedTextView).text = location.toString()
+    }
+
+    override fun locationError(msg: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun checkRequiredLocationPermission(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
